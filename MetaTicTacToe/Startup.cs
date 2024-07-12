43168f9b8d6b5ea2b0f55db327703610523bd1b6
Stefan Insam <ramsesoriginal@gmail.com>
2024-07-12 21:53:18 +0200

    swagger using xml docs

--- MetaTicTacToe/Startup.cs
+++ MetaTicTacToe/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 namespace MetaTicTacToe
 {
@@ -28,6 +29,11 @@ namespace MetaTicTacToe
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "MetaTicTacToe API", Version = "v1" });
+
+                // Set the comments path for the Swagger JSON and UI
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
             });
         }
 
